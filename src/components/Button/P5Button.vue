39c214580dcf55fed748f888e51b0b8af7a30d0a
Brandon Chung <deltaharbinger@gmail.com>
2024-01-22 08:43:46 -0500

    Added button default outline

--- src/components/Button/P5Button.vue
+++ src/components/Button/P5Button.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <button class="relative font-semibold p-4 text-xl">
+    <button class="relative bg-white font-semibold p-4 text-xl hover:scale-110">
         <slot />
         <span class="shimmer bg-white absolute p-4 inset-0 m-auto">
             <slot />
@@ -13,23 +13,23 @@
 
 <style scoped>
 button:hover {
-    filter: drop-shadow(4px 4px 0px rgb(222, 4, 4));
+    filter: drop-shadow(4px 4px 0px #f00b0b);
     animation: shimmer-shadow 200ms linear forward;
-    transition: all 200ms 50ms;
+    transition: all 200ms 100ms;
 }
 
 .shimmer {
     clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
     -webkit-clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
     opacity: 0;
+    filter: invert();
+    transition: opacity 500ms, clip-path 200ms;
+    animation: shimmering 0.5s linear infinite;
+    -webkit-animation: shimmering 0.5s linear infinite;
 }
 
 button:hover .shimmer {
     opacity: 1;
-    animation: shimmering 0.5s linear infinite;
-    -webkit-animation: shimmering 0.5s linear infinite;
-    transition: clip-path 200ms, opacity 50ms;
-    filter: invert();
 }
 
 @keyframes shimmering {
