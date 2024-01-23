9df0f36d9e4dd68392322fdf48ebb03bbb03d4d6
Brandon Chung <deltaharbinger@gmail.com>
2024-01-22 22:32:04 -0500

    Shimmer is now standalone css

--- src/components/Button/P5Button.stories.ts
+++ src/components/Button/P5Button.stories.ts
@@ -1,6 +1,8 @@
-import type { Meta, StoryFn, StoryObj } from '@storybook/vue3';
+import type { Meta, StoryFn } from '@storybook/vue3';
+import { withActions } from "@storybook/addon-actions/decorator";
 
 import P5Button from './P5Button.vue';
+import { ref } from 'vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
@@ -10,40 +12,49 @@ const meta = {
   argTypes: {
   },
   args: { }, // default value
+  parameters: {
+    actions: {
+      handles: ['mouseover', 'click', 'dblclick']
+    }
+  },
+  decorators: [withActions],
 } satisfies Meta<typeof P5Button>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
-const DefaultButton: StoryFn = () => ({
+const DefaultButton: StoryFn = (args) => ({
     components: { P5Button },
     setup: () => {
-        return {}
+        return { args }
     },
     template: `
-        <P5Button>Hello</P5Button>
+        <P5Button v-bind="args" v-on="args.eventHandlers">Hello</P5Button>
+        
     `
+})
+
+const ToggleButton: StoryFn = (args) => ({
+  components: { P5Button },
+  setup: () => {
+      const toggleState = ref(false)
+      return { args, toggleState }
+  },
+  template: `
+      <P5Button v-bind="args" v-model="toggleState">Hello</P5Button>
+      <br class="mb-8" />
+      <pre class="transition-colors duration-500" :class="{'text-green-600': toggleState, 'text-red-600': !toggleState}">Toggle State: {{ toggleState ? 'On' : 'Off' }}</pre>
+  `
 })
 
 export const Default = DefaultButton.bind({})
 
+export const Active = DefaultButton.bind({})
+Active.args = {
+  active: true,
+}
 
-export const Primary: Story = {
-  args: {
-  },
-};
+export const Toggle = ToggleButton.bind({})
+Toggle.args = {
+  toggle: true
+}
 
-export const Secondary: Story = {
-  args: {
-  },
-};
-
-export const Large: Story = {
-  args: {
-  },
-};
-
-export const Small: Story = {
-  args: {
-  },
-};
--- src/components/Button/P5Button.vue
+++ src/components/Button/P5Button.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+const props = defineProps<{ active?: boolean, toggle?: boolean }>()
 
+const emit = defineEmits(['click'])
+
+const toggleState = defineModel({ default: false, type: Boolean, required: false })
+
+const onClick = () => {
+    props.toggle && (toggleState.value = !toggleState.value)
+    emit('click')
+}
 </script>
 
 <template>
-    <button class="relative bg-white font-semibold p-4 text-xl hover:scale-110">
+    <button
+        class="shimmer-base relative bg-white font-semibold p-4 text-xl hover:scale-110"
+        :class="{ 'shimmer-active': active || (toggle && toggleState), 'toggle-hover': toggle }"
+        @click="onClick"
+    >
         <slot />
         <span class="shimmer bg-white absolute p-4 inset-0 m-auto">
             <slot />
@@ -12,55 +25,7 @@
 </template>
 
 <style scoped>
-button:hover {
-    filter: drop-shadow(4px 4px 0px #f00b0b);
-    animation: shimmer-shadow 200ms linear forward;
-    transition: all 200ms 100ms;
-}
-
-.shimmer {
-    clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
-    -webkit-clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
-    opacity: 0;
-    filter: invert();
-    transition: opacity 500ms, clip-path 200ms;
-    animation: shimmering 0.5s linear infinite;
-    -webkit-animation: shimmering 0.5s linear infinite;
-}
-
-button:hover .shimmer {
-    opacity: 1;
-}
-
-@keyframes shimmering {
-	0% {
-		-webkit-clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
-		clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
-	}
-	25% {
-		-webkit-clip-path: polygon(13% 13%, 100% 0, 85% 88%, 0 100%);
-		clip-path: polygon(13% 13%, 100% 0, 85% 88%, 0 100%);
-	}
-    50% {
-        -webkit-clip-path: polygon(0 0, 92% 4%, 100% 100%, 2% 96%);
-        clip-path: polygon(0 0, 92% 4%, 100% 100%, 2% 96%);
-    }
-    75% {
-        -webkit-clip-path: polygon(9% 2%, 100% 0, 95% 96%, 0 100%);
-        clip-path: polygon(9% 2%, 100% 0, 95% 96%, 0 100%);
-    }
-	100% {
-		-webkit-clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
-		clip-path: polygon(0 0, 100% 8%, 100% 100%, 14% 95%);
-	}
-}
-
-@keyframes shimmer-shadow {
-    from {
-        filter: drop-shadow(4px 4px 0px rgb(222, 4, 4, 0));
-    }
-    to {
-        filter: drop-shadow(4px 4px 0px rgb(222, 4, 4, 1));
-    }
+.toggle-hover:hover {
+    @apply shimmer-shadow-active
 }
 </style>
